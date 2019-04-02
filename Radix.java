c2b11bc60e8c1062242cd2078ba16ddd9797ecce
angeliquecd <acharles-davis00@stuy.edu>
2019-04-02 10:14:59 -0400

    added comments

--- Radix.java
+++ Radix.java
@@ -3,32 +3,31 @@ public class Radix{
   public static void radixsort(int[]data){
     if (data.length<2) return ;//mine doesn't work with length 1
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-  for (int i= 0;i<20;i++){
-    buckets[i]= new MyLinkedList<Integer>();}
+  for (int i= 0;i<20;i++){buckets[i]= new MyLinkedList<Integer>();}
     MyLinkedList<Integer> result = new MyLinkedList<Integer>();
     int largest =0;
     for (int a:data){
-      result.add(a);
-      if (Math.abs(a)>largest) largest=Math.abs(a);
+      result.add(a);//creates the result linkedlist to begin with
+      if (Math.abs(a)>largest) largest=Math.abs(a);//simultaneously figures out where to stop
     }
     System.out.println("the linkedlist: "+result);
-    int n = 1;
-    while(largest>=n){
+    int n = 1;//represents the place you're looking at
+    while(largest>=n){//tells you where to start
   //    System.out.println(largest);
     //  System.out.println(result.hasNext()+" "+result.size());
       int passes=0;
-    while(passes<data.length){
-      int workingwith = result.removeFront();
+    while(passes<data.length){//goes through each one
+      int workingwith = result.removeFront();//picks off one by one
       int actual=workingwith;
       int add=0;
       if (workingwith<0){
         add=10;//puts negatives in their place
-        workingwith=Math.abs(workingwith);
+        workingwith=Math.abs(workingwith);//find mod of positive number
       }
       int place = n*10;
       int index = (workingwith%place)/(n)+add;
       System.out.println("working with: "+actual+" index:"+index);
-      buckets[index].add(actual);
+      buckets[index].add(actual);//places stuff in buckets
     //  System.out.println("size: "+result.size());
     passes++;
     }
@@ -36,6 +35,7 @@ public class Radix{
     System.out.println("ended first loop");
     result=new MyLinkedList<Integer>();
     System.out.println(result);
+    //copies back into linkedlist for next pass
     for (int i =19;i>9;i--){
       System.out.println(i+": "+buckets[i]);
       if (buckets[i].hasNext()){//does negatives first
@@ -44,7 +44,7 @@ public class Radix{
   //  buckets[i].clear();}//reforms it into new linked list while also clearing buckets
   }
   System.out.println(result);
-    for (int i=0;i<10;i++){
+    for (int i=0;i<10;i++){//next does positives
       System.out.println(i+": "+buckets[i]);
       if (buckets[i].hasNext()){
       result.extend(buckets[i]);}
@@ -58,6 +58,7 @@ public class Radix{
     data[i]=result.removeFront();
   }
   }
+
   public static void debug(MyLinkedList<Integer>[] buckets){
     int i =0;
   for (MyLinkedList<Integer> a: buckets){
@@ -73,7 +74,7 @@ public static void debug(int[] data){
   System.out.println();
 }
 public static void main (String[] args){
-  int[] data = {1,2,3,10,5,6,3, -5, -8, -220,4300};
+  int[] data = {1,2,3,10,5,6,3, -5, -8, -220,4300,-90000};
   radixsort(data);
   debug(data);
 }
